5efb3d56a140eb27803699f4bd05e870136db667
Melih <doganmmelih@gmail.com>
2023-02-01 08:33:14 +0300

    Finished the backend

---
+++ src/test/java/dev/melih/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.melih.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
